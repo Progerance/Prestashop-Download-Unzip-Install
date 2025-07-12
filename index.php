4589b27e71f98d7b0339f63df76ffefc19f36b16
PROGERANCE <153128450+Progerance@users.noreply.github.com>
2025-07-12 14:16:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file docs/licenses/LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@progerance.fr so we can send you a copy immediately.
+ *
+ * @author    PROGERANCE.COM <contact@progerance.fr> 
+ * @copyright PROGERANCE - Dubois Arnaud
+ * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License ("AFL") v. 3.0
+ */
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
